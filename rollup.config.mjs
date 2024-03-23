ac603cbe917ed9d6d77f70ee6781c0491f6d032c
Marek Miklaszewski <mar.mik1@wp.pl>
2024-03-24 00:53:51 +0100

    Setup Eslint and Prettier

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,22 +1,22 @@
-import typescript from "@rollup/plugin-typescript";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import terser from "@rollup/plugin-terser";
-import babel from "@rollup/plugin-babel";
-import { defineConfig } from "rollup";
-import pkg from "./package.json" assert { type: "json" };
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import babel from '@rollup/plugin-babel';
+import { defineConfig } from 'rollup';
+import pkg from './package.json' assert { type: 'json' };
 
 const config = defineConfig({
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "esm" },
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' },
   ],
   plugins: [
-    babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
     resolve(),
     commonjs({ include: /\/node_modules\// }),
-    typescript({ tsconfig: "./tsconfig.json" }),
+    typescript({ tsconfig: './tsconfig.json' }),
     terser({ output: { comments: false } }),
   ],
   external: Object.keys(pkg.peerDependencies),
