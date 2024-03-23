c81358ccaf2ca220ef2e74ea0bd7efafc8399a73
Marek Miklaszewski <mar.mik1@wp.pl>
2024-03-24 00:21:10 +0100

    Setup Rollup config, add .nvmrc

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,25 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import babel from "@rollup/plugin-babel";
 import { defineConfig } from "rollup";
+import pkg from "./package.json" assert { type: "json" };
 
 const config = defineConfig({
   input: "src/index.ts",
-  output: { file: "dist/index.js", format: "cjs" },
+  output: [
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "esm" },
+  ],
+  plugins: [
+    babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
+    resolve(),
+    commonjs({ include: /\/node_modules\// }),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    terser({ output: { comments: false } }),
+  ],
+  external: Object.keys(pkg.peerDependencies),
 });
 
 export default config;
