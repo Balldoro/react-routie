9a2392a7f0537ebe650a1a56f6012099d0dea2ef
Marek Miklaszewski <mar.mik1@wp.pl>
2024-03-26 23:15:13 +0100

    Create RouteContext

---
+++ src/contexts/RouteContext/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+interface State {
+  children: React.ReactNode;
+  fullPath: string;
+  path: string;
+}
+
+export const RouteContext = React.createContext<State | undefined>(undefined);
+
+interface RouteContextProviderProps {
+  children: React.ReactNode;
+}
+
+export const RouteContextProvider = ({
+  children,
+}: RouteContextProviderProps) => {
+  if (React.isValidElement(children)) {
+    const { parentPath, path, children: nestedChildren } = children.props;
+    const fullPath = `${parentPath}${path}`;
+
+    const value = { children: nestedChildren, fullPath, path };
+
+    return (
+      <RouteContext.Provider value={value}>{children}</RouteContext.Provider>
+    );
+  }
+};
+
+export const useRoute = () => {
+  const context = React.useContext(RouteContext);
+
+  if (!context) {
+    throw new Error('useRoute used outside of the Provider!');
+  }
+
+  return context;
+};
