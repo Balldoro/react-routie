da4f164f50aea1c15873b80717c56b6df6432ce7
Marek Miklaszewski <mar.mik1@wp.pl>
2024-03-28 10:27:55 +0100

    Add interface for styling active links (#9)

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,23 +1,41 @@
 import * as React from 'react';
 import { useNavigate } from 'hooks';
+import { useRoute, useRouter } from 'contexts';
+import { mergePaths } from 'utils';
+
+interface StylesProps {
+  isActive: boolean;
+}
 
 interface LinkProps
-  extends React.DetailedHTMLProps<
-    React.AnchorHTMLAttributes<HTMLAnchorElement>,
-    HTMLAnchorElement
-  > {
+  extends Omit<React.ComponentPropsWithRef<'a'>, 'style' | 'className'> {
   path: string;
   replace?: boolean;
+  style?: React.CSSProperties | ((props: StylesProps) => React.CSSProperties);
+  className?: string | ((props: StylesProps) => string);
 }
 
 export const Link = ({
   path,
   replace = false,
   children,
+  className,
+  style,
   onClick,
   ...props
 }: LinkProps) => {
+  const { fullRoutePath } = useRoute();
+  const { currentPath } = useRouter();
   const navigate = useNavigate();
+
+  const fullLinkPath = path.startsWith('/')
+    ? path
+    : mergePaths(fullRoutePath, '/', path);
+
+  const isActive = currentPath === fullLinkPath;
+
+  const getStylesWithProps = <T,>(styles: T) =>
+    styles instanceof Function ? styles({ isActive }) : styles;
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
@@ -26,7 +44,13 @@ export const Link = ({
   };
 
   return (
-    <a href={path} onClick={handleClick} {...props}>
+    <a
+      href={path}
+      onClick={handleClick}
+      style={getStylesWithProps(style)}
+      className={getStylesWithProps(className)}
+      {...props}
+    >
       {children}
     </a>
   );
